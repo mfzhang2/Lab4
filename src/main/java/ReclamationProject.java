bd7020e0ec0bd511f25b7ecaf293aa13c5ee4ef3
Michael Zhang <Michael Zhang@LAPTOP-OC95N3KG>
2017-09-27 12:31:06 -0500

    Finished lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,44 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * @author      Michael Zhang mfzhang2@illinois.edu
+ * @version     1.1
+ */
+public class ReclamationProject {
+    /**
+     * Returns a sequence of characters that the two words share.
+     *
+     * If the two words are the same, the function will return an empty string;
+     * if the two words are different, return the longest sequence of characters
+     * that the two words share.
+     *
+     * @param   a       word one that is entered in
+     * @param   b       word two that is entered in
+     * @return  overlap the sequence of characters that the two words share
+     */
+    public static String doit(final String a, final String b) {
+        String word1 = a;
+        String word2 = b;
+        String overlap = "";
+        // swap word1 and word2 if the length of word1 is greater than the length of word2
+        if (word1.length() > word2.length()) {
+            String temp = word2;
+            word2 = word1;
+            word1 = temp;
+        }
+        // if word1 equals word2, set their overlapping characters to an empty string
+        if (word1.equals(word2)) {
+            overlap = "";
+        }
+        // if the two words contain matching sequence of characters, set r = to that sequence
+        for (int i = 0; i < word2.length(); i++) {
+            for (int j = i; j < word2.length(); j++) {
+                if (word1.contains(word2.substring(i, j))) {
+                    overlap = word2.substring(i, j);
+                }
+            }
+        }
+        return overlap;
+    }
 }
